954bbf12d5260fcb18ab7eec12d9beaacf9f5627
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-07-30 09:46:09 +0300

    update app css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading";
-import AddFavourites from "./components/AddFavourites";
 import SearchBox from "./components/SearchBox";
-
+import AddFavourites from "./components/AddFavourites";
+import RemoveFavourites from "./components/RemoveFavourites";
 
 
 const App = () => {
@@ -44,31 +44,31 @@ const saveToLocalStorage = (items) => {
 
  
  
-	return (
-		<div className='container-fluid movie-app'>
-				  <div className='row d-flex align-items-center mt-4 mb-4'>
-					  <MovieListHeading heading='Movies' />
-					  <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-				  </div>
-				  <div className='row'>
-					  <MovieList
-						  movies={movies}
-						  handleFavouritesClick={addFavouriteMovie}
-						  favouriteComponent={AddFavourites}
-					  />
-				  </div>
-				  <div className='row d-flex align-items-center mt-4 mb-4'>
-					  <MovieListHeading heading='Favourites' />
-				  </div>
-				  <div className='row'>
-					  <MovieList
-						  movies={favourites}
-						  handleFavouritesClick={removeFavouriteMovie}
-						  favouriteComponent={RemoveFavourites}
-					  />
-				  </div>
-			  </div>
-		  );
-	  };
-	  
-	  export default App;
+return (
+  <div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					handleFavouritesClick={addFavouriteMovie}
+					favouriteComponent={AddFavourites}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={favourites}
+					handleFavouritesClick={removeFavouriteMovie}
+					favouriteComponent={RemoveFavourites}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default App;
