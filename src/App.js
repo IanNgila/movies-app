73ed2faf12d63d0d3f65b2ec01d0f223d07a349f
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-07-30 09:28:14 +0300

    importing second component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState ,useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
 
 
 
