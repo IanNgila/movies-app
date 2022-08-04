357926e4d8fcfa4ac400b29b7763e198dbd2602c
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-08-04 09:26:27 +0300

    done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
 import AddFavourites from "./components/AddFavourites";
 import RemoveFavourites from "./components/RemoveFavourites";
-// import { Routes } from "react-router-dom";
 
 
 const App = () => {
@@ -14,66 +13,54 @@ const App = () => {
   const [movies, setMovies] = useState([]);
   const [favourites, setFavourites] = useState([])
   const [searchValue, setSearchValue] = useState([])
-  
+
   useEffect(() => {
-    fetch('https://pollen-storm-ulna.glitch.me/movie')
+    fetch('http://localhost:5000/movie')
     .then((res) => res.json())
     .then((data) => setMovies(data))
 },[searchValue])
 
 
 const saveToLocalStorage = (items) => {
-		localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
-	};								
-
-	const addFavouriteMovie = (movie) => {
-		const newFavouriteList = [...favourites, movie];
-		setFavourites(newFavouriteList);
-		saveToLocalStorage(newFavouriteList);
-	};
-
-	const removeFavouriteMovie = (movie) => {
-		const newFavouriteList = favourites.filter(
-			(favourite) => favourite.imdbID !== movie.imdbID
-		);
-
-		setFavourites(newFavouriteList);
-		saveToLocalStorage(newFavouriteList);
-	};
-
-  
-
- 
- 
+        localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+    };
+    const addFavouriteMovie = (movie) => {
+        const newFavouriteList = [...favourites, movie];
+        setFavourites(newFavouriteList);
+        saveToLocalStorage(newFavouriteList);
+    };
+    const removeFavouriteMovie = (movie) => {
+        const newFavouriteList = favourites.filter(
+            (favourite) => favourite.imdbID !== movie.imdbID
+        );
+        setFavourites(newFavouriteList);
+        saveToLocalStorage(newFavouriteList);
+    };
 return (
-  
-	<div className='container-fluid movie-app'>
-			<div className='row d-flex align-items-center mt-4 mb-4 '>
-				
-				<MovieListHeading heading='Movies Watchlist' />
-				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
-			</div>
-			<div className='row'>
-			<div className="col-md-3"></div>
-				<MovieList
-					movies={movies}
-					handleFavouritesClick={addFavouriteMovie}
-					favouriteComponent={AddFavourites}
-				/>
-			</div>
-			<div className='row d-flex align-items-center mt-4 mb-4'>
-				<MovieListHeading heading='Favourites' />
-			</div>
-			<div className='row'>
-			<div className="col-md-3"></div>
-				<MovieList
-					movies={favourites}
-					handleFavouritesClick={removeFavouriteMovie}
-					favouriteComponent={RemoveFavourites}
-				/>
-			</div>
-		</div>
-	);
+  <div className='container-fluid movie-app'>
+            <div className='row d-flex align-items-center mt-4 mb-4'>
+                <MovieListHeading heading='Movies Watchlist' />
+                <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+            </div>
+            <div className='row'>
+                <MovieList
+                    movies={movies}
+                    handleFavouritesClick={addFavouriteMovie}
+                    favouriteComponent={AddFavourites}
+                />
+            </div>
+            <div className='row d-flex align-items-center mt-4 mb-4'>
+                <MovieListHeading heading='Favourites' />
+            </div>
+            <div className='row'>
+                <MovieList
+                    movies={favourites}
+                    handleFavouritesClick={removeFavouriteMovie}
+                    favouriteComponent={RemoveFavourites}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default App;
